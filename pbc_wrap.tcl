798a41a6e4f7c3e6d51760971d4180a4a843292a
Narin Lawan <40512669+N-Lawan@users.noreply.github.com>
2022-06-08 15:22:13 +0700

    Add files via upload

---
+++ pbc_wrap.tcl
@@ -0,0 +1,5 @@
+pbc wrap -center com -centersel "protein and segname CA1" -compound residue -all
+
+pbc wrap -center com -centersel "protein and segname CB1" -compound residue -all
+
+pbc wrap -center com -centersel "protein " -compound residue -all
